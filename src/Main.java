00cd998809bb6c3727498fcd9aa671f3a09d1ab0
Francelino <fmarcilio1989@gmail.com>
2022-08-02 17:37:19 -0300

    imprimindo o resultado dos metodos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -9,12 +11,29 @@ public class Main {
         curso1.setDescricao("Desafio de java para a dio");
         curso1.setTitulo("Curso java");
         curso1.setCargaHoraria(4);
-        System.out.println(curso1);
+//        System.out.println(curso1);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de java");
         mentoria.setDescricao("Ajudando devs com dúvidas");
         mentoria.setData(LocalDate.now());
-        System.out.print(mentoria);
+//        System.out.print(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do curso top");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devFran = new Dev();
+        devFran.setNome("Francelino");
+        devFran.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + devFran.getConteudosInscritos());
+
+        devFran.progredir();
+
+        System.out.println("Conteúdos Inscritos " + devFran.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devFran.getConteudosConcluidos());
+        System.out.println("Conteúdos Concluídos " + devFran.calcularTotalXp());
     }
 }
